16d2907f8caaeca2a97a4a6177f3161e5d9bd1af
centrinoLaptop <centrinoLaptop@DESKTOP-DK2469P>
2025-12-27 01:36:08 +0200

    Add project files.

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF1
+{
+    internal class ApplicationDbContext : DbContext
+
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server = DESKTOP-DK2469P; Database= Asp;TrustServerCertificate = True;Trusted_Connection=True");
+        }
+    }
+}
